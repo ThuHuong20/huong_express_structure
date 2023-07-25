47fc918150aeaff50894c8ae20525a301d526f03
ThuHuong20 <huong210720@gmail.com>
2023-07-25 14:01:15 +0700

    update

--- src/server.js
+++ src/server.js
@@ -6,6 +6,11 @@ dotenv.config();
 import express from 'express'
 const server = express();
 
+// Connect my sql
+import mySQL from './databases/mySQL';
+import { mysqlConnect } from './databases/mySQL'
+mysqlConnect();
+
 server.listen(process.env.SERVER_PORT, () => {
     console.log(`Server is running on: ${process.env.SERVER_HOST}:${process.env.SERVER_PORT}/`)
 })
