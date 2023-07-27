8b09082ba0cab0e53a31e019e9032a30b3dc1d19
ThuHuong20 <huong210720@gmail.com>
2023-07-27 17:05:43 +0700

    finish category

--- src/controllers/category.contronller.js
+++ src/controllers/category.contronller.js
@@ -19,6 +19,49 @@ module.exports = {
                 message: 'loi khong xac dinh'
             })
         }
-    }
+    },
+    readMany: async function (req, res) {
+        // console.log("🚀 ~ file: category.model.js:6 ~ newCategory:", newCategory)
+        console.log("da vao contronller readme", req.query.status);
+
+        try {
+            let result = await categoryModel.readMany(req.query.status)
+            if (result.status) {
+                return res.status(200).json({
+                    message: result.message,
+                    data: result.data
+                })
+            }
+            return res.status(500).json({
+                message: result.message
+            })
+        } catch (err) {
+            return res.status(500).json({
+                message: 'loi khong xac dinh'
+            })
+        }
+
+    },
+    update: async function (req, res) {
+        // console.log("categoryId", req.params.categoryId);
+        // console.log("req.body", req.body);
+        try {
+            let result = await categoryModel.update(req.params.categoryId, req.body)
+            if (result.status) {
+                return res.status(200).json({
+                    message: result.message,
+                    data: result.data
+                })
+            }
+            return res.status(500).json({
+                message: result.message
+            })
+        } catch (err) {
+            console.log("🚀 ~ file: category.contronller.js:60 ~ err:", err)
+            return res.status(500).json({
+                message: 'loi khong xac dinh 1'
+            })
+        }
+    },
 
 }
--- src/routes/apis/category.api.js
+++ src/routes/apis/category.api.js
@@ -4,5 +4,7 @@ const router = express.Router();
 import categoryController from '../../controllers/category.contronller'
 import categoryMiddleware from '../../middlewares/category.middleware';
 router.post('/', categoryMiddleware.createValidate, categoryController.create)
+router.get('/', categoryMiddleware.readManyValidate, categoryController.readMany)
+router.patch('/:categoryId', categoryMiddleware.updateValidate, categoryController.update)
 
 module.exports = router;
